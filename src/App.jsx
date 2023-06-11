edee998fa62691eb20f8629b11aedcf0897286b3
ghousalik97 <ghousali11@gmail.com>
2023-06-11 11:53:59 +0800

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useContext } from 'react';
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Write from './pages/Write';
+import Single from './pages/Single';
+import { AuthContext } from './context/authContext';
+
+
+
+
+function App() {
+
+    const { currentUser } = useContext(AuthContext);
+    const router = createBrowserRouter(
+        [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            },
+            {
+                path: "/register",
+                element: <Register />
+            },
+            {
+                path: "/write",
+                element: currentUser ? <Write /> : <Navigate to="/login" replace />
+            }, {
+                path: "/post/:id",
+                element: <Single />
+            }
+
+        ]
+    );
+
+    return (<div className="app">
+        <div className='container'>
+            <RouterProvider router={router} />
+        </div>
+    </div>);
+
+
+}
+
+export default App;
